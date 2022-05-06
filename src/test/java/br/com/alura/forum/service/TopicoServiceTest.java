4c0bf73c7c5cda4a4d1f65bfcad64f3db34cc7ff
caio.nepos <caio.nepos@gmail.com>
2022-05-06 11:40:22 -0300

    ultima atualizacao antes do update de versao do spring boot

--- src/test/java/br/com/alura/forum/service/TopicoServiceTest.java
+++ src/test/java/br/com/alura/forum/service/TopicoServiceTest.java
@@ -1,40 +1,66 @@
 package br.com.alura.forum.service;
 
-import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
-@RunWith(SpringRunner.class)
+
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class TopicoServiceTest {
-
-	@Autowired
+	
+	@InjectMocks
 	private TopicoService topicoService;
 	
-	@Autowired
+	@Mock
 	private TopicoRepository topicoRepository;
 	
-	@Test
-	public void deveriaListarTodosTopicos() {
-		String nomeCurso = "";
-		Assert.assertNotNull(topicoService.lista(nomeCurso));
-		
-		
+	@Mock
+	private CursoRepository cursorepository;
+
+	@Mock
+	private TopicoDto topicoDto;
+	
+	@Mock
+	private Topico topico;
+	
+	private Curso curso;
+	
+	@Mock 
+	private List<TopicoDto> lista;
+	
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
-	public void deveriaListarApenasTopicosDoNomeCurso() {
-		String nomeCurso = "";
-		Assert.assertNotNull(topicoService.lista(nomeCurso));
-		//Assert.assert
-		
+	public void deveriaListarTodosTopicos() {
+		Mockito.when(topicoService.lista("")).thenReturn(startTopico());
+		Assert.assertNotNull(startTopico());
+	}
+
+	private List<TopicoDto> startTopico() {
+		topico = new Topico("Erro no codigo", "Mensagem teste", curso);
+		topicoDto = new TopicoDto(topico);
+		return (List<TopicoDto>) topicoDto;
 	}
 }
